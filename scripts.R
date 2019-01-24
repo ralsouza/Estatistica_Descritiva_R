128bd588aeaaa499b49691e68c40909dfc70711f
ralsouza <ls.rafael@icloud.com>
2019-01-24 17:37:16 -0200

    Nova tabela adicionada.

--- scripts.R
+++ scripts.R
@@ -1,26 +1,40 @@
 
 
-df_ferimentos_graves <-  data.frame(tipo    = c('Sem Vítimas','Com Ferimentos Graves','Com Ferimentos Leves','Com Mortos'),
-                                     media   = apply(mt_acidentes,2, mean),
-                                     d.padr  = apply(mt_acidentes,2, sd),
-                                     var     = apply(mt_acidentes,2, var),
-                                     mediana = apply(mt_acidentes,2, median))
-
-# Remove os nomes das linhas
-row.names(df_ferimentos_graves2) <- NULL
-
-# Renomeia os nomes das colunas
-colnames(df_ferimentos_graves2) <- c('Tipo','Média','Desvio Padrão', 'Variância', 'Mediana')
-
-library(ggplot2)
-
-ggplot(df_ferimentos_graves, aes(x=tipo, y=media)) + 
-  geom_errorbar(aes(ymin=media-d.padr, ymax=media+d.padr), width=.2) +
-  geom_line() +
-  geom_point()
 
 
-?kable
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mt_bioma <- matrix(c(176496, 6210, 3340, 1607, 122682, 20974, 7658, 14025,
+                         103835, 0   , 0   , 428 , 10980 , 2033 , 394 , 0    ,
+                         58636 , 6210, 3340, 1179, 21702 , 18940, 7264, 0   ),
+                       nrow = 8,
+                       ncol = 3)
+
+# Nomeando os nomes das variáveis e observações. 
+row.names(mt_bioma) <- c('Área Total',
+                             'Floresta Estacional Semidecidual',
+                             'Floresta Estacional Decidual',
+                             'Savana Estépica',
+                             'Estepe',
+                             'Formações pioneiras',
+                             'Contatos entre tipos de vegetação',
+                             'Superfície com água')
+colnames(mt_bioma) <- c('Em Km²','Em Km²','Em Km²')
 
 
